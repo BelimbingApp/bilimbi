0c0208aaeea3ffca2c3f2aa7f55bee8dd92dc68b
kiatng <kiatsiong.ng@gmail.com>
2026-08-17 21:31:47 +0800

    fix(core/compatibility): update migration discovery test with new employee types check migration (#214)

--- apps/core/compatibility/test/migration_discovery_test.exs
+++ apps/core/compatibility/test/migration_discovery_test.exs
@@ -57,7 +57,8 @@ defmodule Bilimbi.Core.Compatibility.MigrationDiscoveryTest do
              Bilimbi.Core.Employee.Migrations.CreateCompatibilityBaseline,
              Bilimbi.Core.User.Migrations.CreateCompatibilityBaseline,
              Bilimbi.Base.Audit.Migrations.CreateCompatibilityBaseline,
-             Bilimbi.Core.Employee.Migrations.AdaptEmployeeTypesTenancyIndexes
+             Bilimbi.Core.Employee.Migrations.AdaptEmployeeTypesTenancyIndexes,
+             Bilimbi.Core.Employee.Migrations.AddEmployeeTypesCustomCompanyCheck
            ]
 
     assert Enum.map(entries, &elem(&1, 2)) == [
@@ -72,6 +73,7 @@ defmodule Bilimbi.Core.Compatibility.MigrationDiscoveryTest do
              :compatible_baseline,
              :compatible_baseline,
              :compatible_baseline,
+             :bilimbi_only,
              :bilimbi_only
            ]
 
